9a86f8d221300bcfd315e3db81abb1c87c0b74f2
Gaftoniuc Vlad <Vlad@Gaftoniucs-MacBook-Air.local>
2020-06-04 22:16:14 +0300

    My own spin

--- get-quote.py
+++ get-quote.py
@@ -2,12 +2,19 @@ import random
 
 def quote_bot():
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
+  with open("quotes.txt") as f:
+    quotes = f.readlines()
 
-  last = len(quotes) - 1   
-  print(quotes[random.randint(0, last)])
+  control = int(input("How many random quotes would you like to see ? "))
+  last = len(quotes) - 1
+
+  if control in range(0, last):
+    end = control
+  else:
+    end = last
+
+  for c in range(0, end):
+    print('"' + quotes[random.randint(0, last)].strip("\n") + '"')
 
 if __name__== "__main__":
   quote_bot()
